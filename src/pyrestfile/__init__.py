e4780381f3c6b483a89f2f59691aa49d671c2412
Abel Castillo <abelcastillomath@gmail.com>
2025-04-16 18:33:18 -0500

    fix typo in init, and in toml

--- src/pyrestfile/__init__.py
+++ src/pyrestfile/__init__.py
@@ -4,7 +4,7 @@ from pyrestfile.parser import parse_rest_file
 from pyrestfile.request_block_grammar import HTTPRequest
 
 try:
-    __version__ = version(__package__ or "pyrestfileparser")
+    __version__ = version(__package__ or "pyrestfile")
 except PackageNotFoundError:
     __version__ = "0.0.0"
 
